8268ec93d0e114f97c778eb73d3087b16004b77e
Karl Kode <marc@marcg.ch>
2013-10-26 01:34:38 +0200

    Fix for AttirbuteError if request.authorization is None

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ from models import User
 
 def check_auth(*args, **kwargs):
     auth = request.authorization
+    if not auth:
+        return False
     user = User.query.filter(User.email == auth.username, User.password == auth.password).first()
     if not user:
         return False
