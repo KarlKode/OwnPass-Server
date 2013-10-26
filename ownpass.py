f9b50657843630f401370eab4b361f7337e5be06
Karl Kode <marc@marcg.ch>
2013-10-26 05:12:24 +0200

    Bugfix

--- ownpass.py
+++ ownpass.py
@@ -15,7 +15,7 @@ api = Api(app)
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Methods', 'DELETE, GET, HEAD, OPTIONS, POST, PUT')
-    response.headers.add('Access-Control-Allow-Headers', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Authorize, Content-Type')
     return response
 
 # Debug stuff
