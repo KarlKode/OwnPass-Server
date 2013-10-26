36d94c015724eb9987a924f0f3b103d041e26bcd
Karl Kode <marc@marcg.ch>
2013-10-26 03:37:51 +0200

    Rewrote the rest stuff from flask-rest to flask-restful

--- passwords.py
+++ passwords.py
@@ -1,34 +1,70 @@
 # -*- coding: utf-8 -*-
-from flask import request
-from flask.ext.rest import need_auth
+from flask import request, abort, g
+from flask.ext.restful import Resource, reqparse, marshal_with
 from db import db
 from models import Password
-from utils import check_auth
+from utils import auth_required
 
 
-class PasswordHandler(object):
-    @need_auth(check_auth, 'user', remove_attr=False)
-    def add(self, user):
-        # Validate request
-        if not (request.json and request.json.has_key('url') and request.json.has_key('username') and request.json.has_key('password')):
-            return 400, 'Invalid request'
-        # Get JSON request data
-        url = request.json.get('url')
-        username = request.json.get('username')
-        password_string = request.json.get('password')
+password_parser = reqparse.RequestParser()
+password_parser.add_argument('site', type=str, location=('json',))
+password_parser.add_argument('username', type=str, location=('json',))
+password_parser.add_argument('password', type=str, location=('json',))
+
+
+class PasswordListResource(Resource):
+    @auth_required
+    @marshal_with(Password.resource_fields)
+    def get(self):
+        return g.user.passwords.all()
+
+    @auth_required
+    @marshal_with(Password.resource_fields)
+    def post(self):
+        # Check arguments
+        args = password_parser.parse_args()
+        if not args['site'] or not args['username'] or not args['password']:
+            abort(406)
         # Add password
-        password = Password(user.id, url, username, password_string)
+        password = Password(g.user.id, args['site'], args['username'], args['password'])
         db.session.add(password)
         db.session.commit()
-        return 200, password
+        print password.site
+        return password, 201
 
-    def update(self):
-        pass
+class PasswordResource(Resource):
+    @auth_required
+    @marshal_with(Password.resource_fields)
+    def get(self, password_id):
+        password = Password.get_or_404(password_id)
+        if password.user_id != g.user.id:
+            abort(404)
+        return password
 
-    def delete(self):
-        pass
+    @auth_required
+    def delete(self, password_id):
+        # Users can only delete their own passwords
+        password = Password.get_or_404(password_id)
+        if password.user_id != g.user.id:
+            abort(404)
+        # Delete password from the database
+        db.session.delete(password)
+        db.session.commit()
+        return '', 204
 
-    @need_auth(check_auth, 'user', remove_attr=False)
-    def list(self, user):
-        passwords = user.passwords.all()
-        return 200, passwords
+    @auth_required
+    @marshal_with(Password.resource_fields)
+    def put(self, password_id):
+        # Users can only edit their own passwords
+        password = Password.get_or_404(password_id)
+        if password.user_id != g.user.id:
+            abort(404)
+        # Check arguments
+        args = password_parser.parse_args()
+        if not args['site'] or not args['username'] or not args['password']:
+            abort(406)
+        password.site = args['site']
+        password.username = args['username']
+        password.password = args['password']
+        db.session.commit()
+        return password, 201
--- utils.py
+++ utils.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import request
+from flask import request, abort, g
+from functools import wraps
 
 from models import User
 
 
-def check_auth(*args, **kwargs):
-    auth = request.authorization
-    if not auth:
-        return False
-    user = User.query.filter(User.email == auth.username, User.password == auth.password).first()
-    if not user:
-        return False
-    return user
+def auth_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not request.authorization:
+            return abort(401)
+        auth = request.authorization
+        user = User.query.filter(User.email == auth.username, User.password == auth.password).first()
+        if not user:
+            return abort(401)
+        g.user = user
+        return f(*args, **kwargs)
+    return decorated_function
